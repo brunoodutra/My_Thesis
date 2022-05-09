2212b4aef8fd2de83153c3ea1363f741b09c9a73
brunoodutra <brunodutra60@.com>
2022-05-08 21:21:24 -0300

    update open_emg file

--- Codes/Open_emg_and_force_data.py
+++ Codes/Open_emg_and_force_data.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 20 22:09:25 2019
-
+Created on Tue April 20 22:09:25 2022
 @author: bruno
 """
 
+# Open libraries 
 import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt 
@@ -14,12 +14,13 @@ import seaborn as ns;
 #import Ident as id
 from sklearn.model_selection import cross_val_score
 
-
+# read the datalog that is in " .m " file 
 mat = scipy.io.loadmat('EMG_force_datalog/recording_force_v1.mat')     
 emg = mat['emg'];
 force=  mat['Force'];
 Ts= mat['Ts'];
-   
+
+# define th Signal EMG with the imput signal 
 u1=emg[:,0];
 u2=emg[:,1];
 u3=emg[:,2];
@@ -28,15 +29,20 @@ u5=emg[:,4];
 u6=emg[:,5];
 u7=emg[:,6];
 u8=emg[:,7];
+
+#Define the grasping force with the output signal 
 y=force;
 y.shape=(-1,1)
 nit=y.shape[0]
 
 
 #%%
+# sample time 
 Ts=0.005
+# datalog recording time 
 t  =np.linspace(1, Ts*nit-Ts, nit)
-   
+
+# plot the EMG and Force  signals    
 fig, axs = plt.subplots(8, 1)
 fig.suptitle('Dados MYO')
 axs[0].plot(t, u1, 'y')
