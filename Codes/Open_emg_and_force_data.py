8fdb7dccae4c3c0795b8a4369b7ba14b5bc50861
brunoodutra <brunodutra60@.com>
2022-05-10 01:10:43 -0300

    update visualization

--- Codes/Open_emg_and_force_data.py
+++ Codes/Open_emg_and_force_data.py
@@ -44,14 +44,36 @@ Ts=0.005
 t  =np.linspace(1, Ts*nit-Ts, nit)
 
 # plot the EMG and Force  signals    
-fig, axs = plt.subplots(8, 1)
-fig.suptitle('Dados MYO')
-axs[0].plot(t, u1, 'y')
-axs[1].plot(t, u2, 'b')
-axs[2].plot(t, u3, 'g')
-axs[3].plot(t, u4, 'r')
-axs[4].plot(t, u5, 'y')
-axs[5].plot(t, u6, 'b')
-axs[6].plot(t, u7, 'g')
-axs[7].plot(t, y, 'r')
+fig, axs = plt.subplots(9, 1, figsize=(8, 8))
+fig.suptitle('Datalog')
+
+axs[0].plot(t, u1, '#129845')
+axs[0].set_ylabel('sEMG 1')
+axs[0].set_xlabel('Time(s)')
+
+axs[1].plot(t, u2, '#271254')
+axs[1].set_ylabel('sEMG 2')
+
+axs[2].plot(t, u3, '#FA4411')
+axs[2].set_ylabel('sEMG 3')
+
+axs[3].plot(t, u4, '#098765')
+axs[3].set_ylabel('sEMG 4')
+
+axs[4].plot(t, u5, '#000009')
+axs[4].set_ylabel('sEMG 5')
+
+axs[5].plot(t, u6, 'lightcoral')
+axs[5].set_ylabel('sEMG 6')
+
+axs[6].plot(t, u7, 'orange')
+axs[6].set_ylabel('sEMG 7')
+
+axs[7].plot(t, u8, 'darkblue')
+axs[7].set_ylabel('sEMG 8')
+
+axs[8].plot(t, y, 'red')
+axs[8].set_ylabel('Grasping force')
+
+
 plt.show()
